1dd9fc66e58a3610645af0ba6d3fa732edb6d940
Shaedow2000 <shaedow2000@proton.me>
2026-04-05 11:44:20 +0100

    Add quiz page & Redirect button to quiz page.

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        quiz: resolve(__dirname, "quiz.html"),
+      },
+    },
+  },
 });

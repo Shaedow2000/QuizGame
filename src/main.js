f2fd5dc5362fdde162f963acfec8a834842f18e4
Shaedow2000 <shaedow2000@proton.me>
2026-04-05 14:03:08 +0100

    Fix menu overflow.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const open_menu_button = document.getElementById("menu-button");
 const close_menu_button = document.getElementById("close-menu");
 
 open_menu_button.addEventListener("click", () => {
-  menu.style.right = "10px";
+  menu.style.right = "0px";
   shadow.style.display = "block";
   shadow.style.visibility = "visible";
 });

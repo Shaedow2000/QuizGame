3eae9a594c15d873bf4602a22cc648716796c21b
Shaedow2000 <shaedow2000@proton.me>
2026-04-06 21:46:21 +0100

    Add the next quiz button & Change all the elements variables to contants.

--- src/assets/quizLoader.js
+++ src/assets/quizLoader.js
@@ -7,12 +7,12 @@ let quizId =
 let quizQuestion = quizes[quizId].question;
 let [choice1, choice2, choice3, choice4] = quizes[quizId].choices;
 
-let quizId_span = document.getElementById("quiz-number");
-let quizQuestion_span = document.getElementById("quiz");
-let choice1_button = document.getElementById("choice1");
-let choice2_button = document.getElementById("choice2");
-let choice3_button = document.getElementById("choice3");
-let choice4_button = document.getElementById("choice4");
+const quizId_span = document.getElementById("quiz-number");
+const quizQuestion_span = document.getElementById("quiz");
+const choice1_button = document.getElementById("choice1");
+const choice2_button = document.getElementById("choice2");
+const choice3_button = document.getElementById("choice3");
+const choice4_button = document.getElementById("choice4");
 
 quizId_span.innerHTML = quizId;
 quizQuestion_span.innerHTML = quizQuestion;

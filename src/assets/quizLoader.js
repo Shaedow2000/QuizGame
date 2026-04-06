093da62008f550da63d3751eff31afbd816d0b8b
Shaedow2000 <shaedow2000@proton.me>
2026-04-06 21:39:35 +0100

    Show all the data in the page.

--- src/assets/quizLoader.js
+++ src/assets/quizLoader.js
@@ -7,9 +7,16 @@ let quizId =
 let quizQuestion = quizes[quizId].question;
 let [choice1, choice2, choice3, choice4] = quizes[quizId].choices;
 
-let quizIs_span = document.getElementById("quiz-number");
+let quizId_span = document.getElementById("quiz-number");
 let quizQuestion_span = document.getElementById("quiz");
 let choice1_button = document.getElementById("choice1");
-let chocie2_button = document.getElementById("choice2");
+let choice2_button = document.getElementById("choice2");
 let choice3_button = document.getElementById("choice3");
 let choice4_button = document.getElementById("choice4");
+
+quizId_span.innerHTML = quizId;
+quizQuestion_span.innerHTML = quizQuestion;
+choice1_button.innerHTML = choice1;
+choice2_button.innerHTML = choice2;
+choice3_button.innerHTML = choice3;
+choice4_button.innerHTML = choice4;

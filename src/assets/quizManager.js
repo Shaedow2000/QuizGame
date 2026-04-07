a64ae4308a41845acf0779fb1afc7db9e0288f06
Shaedow2000 <shaedow2000@proton.me>
2026-04-07 21:36:54 +0100

    Correct bug.

--- src/assets/quizManager.js
+++ src/assets/quizManager.js
@@ -6,13 +6,13 @@ const current_quiz =
   localStorage.getItem("current") !== null
     ? localStorage.getItem("current")
     : 0;
-const correct_choice = quizes[current_quiz].corrent;
+const correct_choice = quizes[current_quiz].correct;
 
 for (let i = 0; i < choice_buttons.length; i++) {
   let choice_button = choice_buttons[i];
 
   choice_button.addEventListener("click", () => {
-    if (choice_button.id.includes(current_quiz + 1)) {
+    if (choice_button.id.includes(correct_choice + 1)) {
       console.log("Correct !!");
     } else {
       console.log("Wrong !!");

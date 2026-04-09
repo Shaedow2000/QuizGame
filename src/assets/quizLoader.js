1c6197c6becf0a8a60a2c655e7193fc07fe55f35
Shaedow2000 <shaedow2000@proton.me>
2026-04-09 14:36:26 +0100

    Make a quiz loader function.

--- src/assets/quizLoader.js
+++ src/assets/quizLoader.js
@@ -1,19 +1,23 @@
 import quizes from "./quiz.js";
 
-let quizId = localStorage.getItem("current");
-let quizQuestion = quizes[quizId].question;
-let [choice1, choice2, choice3, choice4] = quizes[quizId].choices;
+export default function load_quiz() {
+  let quizId = localStorage.getItem("current");
+  let quizQuestion = quizes[quizId].question;
+  let [choice1, choice2, choice3, choice4] = quizes[quizId].choices;
 
-const quizId_span = document.getElementById("quiz-number");
-const quizQuestion_span = document.getElementById("quiz");
-const choice1_button = document.getElementById("choice1");
-const choice2_button = document.getElementById("choice2");
-const choice3_button = document.getElementById("choice3");
-const choice4_button = document.getElementById("choice4");
+  const quizId_span = document.getElementById("quiz-number");
+  const quizQuestion_span = document.getElementById("quiz");
+  const choice1_button = document.getElementById("choice1");
+  const choice2_button = document.getElementById("choice2");
+  const choice3_button = document.getElementById("choice3");
+  const choice4_button = document.getElementById("choice4");
 
-quizId_span.innerHTML = quizId;
-quizQuestion_span.innerHTML = quizQuestion;
-choice1_button.innerHTML = choice1;
-choice2_button.innerHTML = choice2;
-choice3_button.innerHTML = choice3;
-choice4_button.innerHTML = choice4;
+  quizId_span.innerHTML = quizId;
+  quizQuestion_span.innerHTML = quizQuestion;
+  choice1_button.innerHTML = choice1;
+  choice2_button.innerHTML = choice2;
+  choice3_button.innerHTML = choice3;
+  choice4_button.innerHTML = choice4;
+}
+
+load_quiz();
